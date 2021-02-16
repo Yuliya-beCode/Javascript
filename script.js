48ff137fc0c003504d30f3290141ebc1d9672f57
Frances Joffany Navarro <frances-joffany-navarro@gmail.com>
2021-02-16 17:03:29 +0100

    Task #29

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
         greetings = ["Hello my Friend!", "Hi darling!", "Hey you!", "Hello there!", "Hello dear friend!"],
         yesAnswers = ["That's fine, because it means that you have a pretty good starting point for tomorrow.", "That's fine: we'll offer you plenty of tips, expertise, and advice, provide you with a hiking rucksack and of course make your time unforgettable.", "And that's fine: Passion for good mood is not something that can be forced on anyone.", "Super, we have great suggestions for you!", "Perfect, it means you would like to follow our tips. Great!"],
         noAnswers = ["But it was as if God said, No, you aren't going to do that.", "So, you are not my friend neither my best friend, you are more than…", "Why don’t you trust me… I could change your mood…", "If you are not sure if these categories apply to you or you have any questions, please don’t hesitate to contact us as we would be delighted to assist you!", "Should I stay with someone who really trust me hmm…"],
-        botImages = ["pictures/pic1.png","pictures/pic2.png","pictures/pic3.png","pictures/pic4.png"];
+        botImages = ["https://assets.stickpng.com/thumbs/5a7b6d93abc3d121aba710a0.png","https://assets.stickpng.com/thumbs/5a7b6dd3abc3d121aba710a3.png","https://assets.stickpng.com/thumbs/58b83c9615d8273a5cab2f94.png","https://assets.stickpng.com/thumbs/58b83cfb15d8273a5cab2f99.png","https://assets.stickpng.com/thumbs/5a7b6d3aabc3d121aba71096.png"];
 
     class Chat {
         constructor(sender, message) {
@@ -46,6 +46,7 @@
     //eventlistener for clicking button
     document.getElementById("send").addEventListener("click", sendMessage);
     document.getElementById("inputMessage").addEventListener("keypress", function(event){
+        console.log(event);
         if(event.key === "Enter" && event.shiftKey){
             input = document.getElementById("inputMessage").value;
             nextLine ="\n";
@@ -101,10 +102,9 @@
             count = 0;
             result = pickRandomMessage(yesAnswers);
             //set attribute for image
-            imageSrc.setAttribute("src", botImages[3]);
+            imageSrc.setAttribute("src", botImages[1]);
             bot.message = yesAnswers[result];
-            return bot.message;    
-
+            return bot.message;
         } else if (answer.search("no") >= 0) {
             count = 0;
             result = pickRandomMessage(noAnswers);
@@ -112,7 +112,13 @@
             imageSrc.setAttribute("src", botImages[2]);
             bot.message = noAnswers[result];
             return bot.message;
-        } else {
+        } else if(answer == ""){
+            result = "I receive an empty message. Please try again."
+            //set attribute for image
+            imageSrc.setAttribute("src", botImages[3]);
+            bot.message = result
+            return bot.message;
+        }else {
             count++;
             //if tried 3 times  
             if (count > 3) {
@@ -122,13 +128,13 @@
                 //remove event listener
                 document.getElementById("send").removeEventListener("click", sendMessage);
                 //set attribute for image
-                imageSrc.setAttribute("src", botImages[1]);
+                imageSrc.setAttribute("src", botImages[4]);
                 bot.message = result
                 return bot.message;
             } else {
                 result = "I'm sorry, I didn't catch what you said. Could you say that again, please?"
                 //set attribute for image
-                imageSrc.setAttribute("src", botImages[1]);
+                imageSrc.setAttribute("src", botImages[4]);
                 bot.message = result
                 return bot.message;
             }
